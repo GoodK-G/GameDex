577d38a58c58daf19bf3a65829ddd17b50fac5e4
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2023-12-12 10:42:38 -0600

    Add review model

---
+++ models/Review.js
@@ -0,0 +1,38 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Review extends Model {};
+
+// Initialize Review model
+Review.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    content: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    rating: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      // Restrict rating on 1 to 10 scale
+      validate: {
+        max: 10,
+        min: 1,
+      },
+    },
+    game_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'game',
+        key: 'id',
+      },
+    },
+  }
+);
+
+module.exports = Review;
