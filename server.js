ee8f8e876ef7400c5f1bfb0f305e3e9f10018c9e
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2023-12-11 21:08:04 -0600

    Add server.js content

---
+++ server.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// Import routes, sequelize connection and helpers for handlebars
+const sequelize = require('./config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Create session and session storage
+const sess = {
+  secret: 'ufPFaq4ozW4TNpNs',
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24,
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+  }),
+};
+
+app.use(session(sess));
+
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`App is now listening at http://localhost:${PORT}`));
+});
