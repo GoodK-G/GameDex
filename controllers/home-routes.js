724185f5a786b5fbb2fe51765959c3c891d253e0
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2023-12-13 16:47:20 -0600

    Add search route for retrieving results from game API

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+require('dotenv').config();
 const { User, Game, Review } = require('../models');
 
 // Get route for homepage
@@ -53,5 +54,58 @@ router.get('/reviews', async (req, res) => {
     res.status(500).json(err);
   };
 });
+
+// Get route for game searches
+// Searches will be requested with name query (e.g., /search?name=Halo)
+router.get('/search', async (req, res) => {
+  try {
+    // Fetch game based on search query
+    const response = await fetch('https://api.igdb.com/v4/games', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Client-ID': process.env.API_ID,
+        'Authorization': `Bearer ${process.env.API_TOKEN}`,
+      },
+      body: `fields name,cover,first_release_date; search "${req.query.name}"; limit 15;`
+    });
+
+    const gameData = await response.json();
+
+    // Set empty game array variable
+    let gameArr = [];
+
+    for (let i = 0; i < gameData.length; i++) {
+      const name = gameData[i].name;
+      const releaseDate = gameData[i].first_release_date;
+
+      // Fetch cover art for each game in search results
+      const response = await fetch('https://api.igdb.com/v4/covers', {
+        method: 'POST',
+        headers: {
+          'Accept': 'application/json',
+          'Client-ID': process.env.API_ID,
+          'Authorization': `Bearer ${process.env.API_TOKEN}`,
+        },
+        body: `fields url; where id = ${gameData[i].cover};`
+      });
+
+      const coverData = await response.json();
+
+      // Compile image link
+      const coverLink = `https://${coverData[0].url}`
+      // Set size of image by replacing part of url link
+      const cover = coverLink.replace('thumb', 'cover_big');
+
+      // Add the array with game info into gameArr array to be used to populate search results
+      gameArr.push([name, cover, releaseDate]);
+    };
+
+    res.json(gameArr);
+  } catch (err) {
+    res.status(500).json(err);
+  };
+})
+
 
 module.exports = router;
