eaa7a197696dfef80307bb6c3d6415c43fc9d48e
Jared Bartos <139281328+jaredbartos@users.noreply.github.com>
2023-12-12 21:03:49 -0600

    Edit connection config

--- config/connection.js
+++ config/connection.js
@@ -2,15 +2,21 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
 // Set up Sequelize connection
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-      host: 'localhost',
-      dialect: 'mysql',
-      port: 3306,
-    },
-  );
+let sequelize
+
+if (process.env.POSTGRES_DB) {
+  sequelize = new Sequelize(process.env.POSTGRES_DB);
+} else {
+  sequelize = new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASSWORD,
+      {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306,
+      },
+    );
+};
 
 module.exports = sequelize;
