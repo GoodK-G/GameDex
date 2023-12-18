6ba64109099c1c2100d5073a5b87207d0a4b7200
Alex Majorowicz <alexmajors0613@gmail.com>
2023-12-18 07:19:11 -0600

    create reviews route

---
+++ controllers/api/reviews.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+const { Review } = require("../../models");
+require("dotenv").config();
+
+//create new review
+router.post("/review/:gameID", async (req, res) => {});
