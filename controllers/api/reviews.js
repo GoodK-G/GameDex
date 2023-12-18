d78ed31aad3679968bfd44276a4f4d74c582b028
Alex Majorowicz <alexmajors0613@gmail.com>
2023-12-18 07:25:44 -0600

    add post route for /review with gameID param

--- controllers/api/reviews.js
+++ controllers/api/reviews.js
@@ -3,4 +3,16 @@ const { Review } = require("../../models");
 require("dotenv").config();
 
 //create new review
-router.post("/review/:gameID", async (req, res) => {});
+router.post("/review/:gameID", async (req, res) => {
+  try {
+    const newReview = await Review.create({
+      rating: req.body.rating,
+      content: req.body.content,
+      user_id: req.session.user_id,
+      game_id: req.params.gameID,
+    });
+    res.json(newReview);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
