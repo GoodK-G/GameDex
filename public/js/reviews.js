57d842048980c118a5dc2bed335b63cf7c24b3fd
Alex Majorowicz <alexmajors0613@gmail.com>
2023-12-19 06:50:58 -0600

    add add review button and functions

---
+++ public/js/reviews.js
@@ -0,0 +1,23 @@
+const addReview = async (e) => {
+  e.preventDefault();
+  const params = window.location.pathname.split("/");
+  const game_id = params[params.length - 1];
+  console.log("game_id", game_id);
+  const rating = document.querySelector("#rating").value;
+  const content = document.querySelector("#content").value;
+  const review = { rating: rating, content: content };
+
+  const response = await fetch(`/api/reviews/${game_id}`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ review }),
+  });
+
+  if (response.ok) {
+    window.location.href = `${window.location.origin}/games/${game_id}`;
+  }
+};
+
+document.getElementById("reviewForm").addEventListener("submit", addReview);
